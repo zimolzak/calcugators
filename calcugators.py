60f640c318772fc1ab2d508d3d944c4d9d7b1395
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-12-07 08:50:20 -0600

    do the iteration/combo

--- calcugators.py
+++ calcugators.py
@@ -1,10 +1,10 @@
-import itertools
+from itertools import combinations
 
-print('n blanks')
-n_blanks = input()
+print('n blanks?')
+n_blanks = int(input())
 
-print('total sum to target')
-target = input()
+print('total sum to target?')
+target = int(input())
 
 print("""Enter the numeric choices.
 Enter 'done' when done.""")
@@ -15,7 +15,15 @@ while(cur_choice != 'done'):
     try:
         c = int(cur_choice)
         choices.append(c)
+        print('ok')
     except ValueError:
-        pass
+        if cur_choice != 'done':
+            print('not a number; skipping')
 
 print(n_blanks, target, choices)
+
+for combo in combinations(choices, n_blanks):
+    if sum(combo) == target:
+        print()
+        print(combo)
+        print(sum(combo))
