7454ff69464cdea1f3a9ddab5856db6294d7b452
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-01-05 09:49:21 -0600

    A little cleaner UI

--- calcugators.py
+++ calcugators.py
@@ -7,23 +7,27 @@ print('total sum to target?')
 target = int(input())
 
 print("""Enter the numeric choices.
-Enter 'done' when done.""")
+Enter 'done' or '' (blank) when done.""")
+terminators = ['done', '']
 choices = []
 cur_choice = None
-while(cur_choice != 'done'):
+while(cur_choice not in terminators):
     cur_choice = input()
     try:
         c = int(cur_choice)
         choices.append(c)
         print('ok')
     except ValueError:
-        if cur_choice != 'done':
+        if cur_choice not in terminators:
             print('not a number; skipping')
 
+print('INPUTS:')
 print(n_blanks, target, choices)
+print()
 
+print('SOLUTIONS:')
+S = set()
 for combo in combinations(choices, n_blanks):
     if sum(combo) == target:
-        print()
-        print(combo)
-        print(sum(combo))
+        S.add(str(combo))
+print('\n'.join([str(x) for x in S]))
